849c6549bb868c39d8435162dab8f79d1230f10e
Jonathan Medeiros <jonathan.info@gmail.com>
2023-10-20 12:19:38 -0300

    Constructor, Wrapper

---
+++ construtor/Construtor.java
@@ -0,0 +1,25 @@
+package construtor;
+
+public class Construtor {
+
+    private int numero;
+
+    public Construtor(int numero) {
+        this.numero = numero;
+        System.out.println("Construtor com 1 parâmetro criado");
+
+    }
+
+    public Construtor(int numero, String texto) {
+        this.numero = numero;
+
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+}
---
+++ construtor/TesteDeConstrutor.java
@@ -0,0 +1,10 @@
+package construtor;
+
+public class TesteDeConstrutor {
+    
+    public static void main(String[] args) {
+        
+        Construtor construtor = new Construtor(1);
+        Construtor construtor1 = new Construtor(2, "Texto");
+    }
+}
